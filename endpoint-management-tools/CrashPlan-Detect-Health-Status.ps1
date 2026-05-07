83607b5b663820a3c6b854829e9f42c71052db36
Andrew Orrison <andrew.orrison@crashplan.com>
2026-05-07 09:11:32 -0500

    Removed ability to collect logs, it's not needed

--- endpoint-management-tools/CrashPlan-Detect-Health-Status.ps1
+++ endpoint-management-tools/CrashPlan-Detect-Health-Status.ps1
@@ -24,35 +24,6 @@ if (Test-Path -Path "C:\ProgramData\CrashPlan\.identity" -PathType Leaf) {
     }
 }
 
-function SendLogs($preFix) {
-    # The link below can be used in a standalone fashion to manually upload files to CrashPlan as well as with this script.
-    $ShareFileLink = "Sharefile request link from CrashPlan Support"
-    $ShareFile = $ShareFileLink.Split("-")[1]
-
-    Copy-Item -Path "$CrashPlanBasePath\log\" -Destination "$CrashPlanBasePath\tmplog\" -Recurse
-
-    $LogFile = "$preFix" + $(hostname) + "-" + $(Get-Date -Format FileDateUniversal) + ".zip"
-    $evtx_name = "$preFix" + $(hostname) + "-System.evtx"
-    $backupLogPath = "$CrashPlanBasePath\tmplog\$evtx_name"
-    $EventViewerLog = Get-CimInstance Win32_NTEventlogFile | Where-Object LogfileName -EQ "System"
-    Invoke-CimMethod -InputObject $EventViewerLog -MethodName BackupEventLog -Arguments @{ ArchiveFileName = $backupLogPath }
-
-    Compress-Archive -Path "$CrashPlanBasePath\tmplog\*" -DestinationPath "$CrashPlanBasePath\$LogFile"
-    $logZip = $CrashPlanBasePath + '\' + $LogFile
-
-    # Uncomment below to upload to a shared folder.
-    # $Destination = "\\netshare\DESTINATION\CrashPlan_Remediation_Logs" + $TimeStamp
-    # Copy-Item -Path $logZip -Destination $Destination -Force
-
-    # Uncomment below to upload to CrashPlan ShareFile
-    # $shareFilePost = (curl.exe -X POST -F File1=@"$logZip" "https://crashplan.sf-api.com/sf/v3/Shares($ShareFile)/Upload?Method=standard&raw=false&fileName=+$LogFile")
-    # $chunkUri = ($shareFilePost | ConvertFrom-Json).ChunkUri
-    # curl.exe -k -F "File1=@$logZip" "$chunkUri"
-
-    Remove-Item $CrashPlanBasePath\tmplog -Recurse
-    Remove-Item $logZip
-}
-
 function Test-FileUnlocked {
     param (
         [Parameter(Mandatory = $true)]
